ddb047de5fe55e322d38e441c0070a34877695f1
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-08-27 15:31:37 +0300

    Refactor weekly saver

--- weekly_saver/weekly_saver.py
+++ weekly_saver/weekly_saver.py
@@ -1,65 +1,66 @@
-
 import os
+from typing import Tuple
 
-import spotipy
-import spotipy.util
+from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 
 
-#####################################################################################################################
-
-
-def get_discover_weekly_playlist(sp: spotipy.Spotify):
+def get_discover_weekly_playlist(spotify_instance: Spotify) -> Tuple[dict, str]:
     dwp = None
-    for p in sp.current_user_playlists()['items']:
-        if p['name'] == 'Discover Weekly':
-            dwp = sp.playlist(p['id'])
+    for playlist in spotify_instance.current_user_playlists()['items']:
+        if playlist['name'] == 'Discover Weekly':
+            dwp = spotify_instance.playlist(playlist['id'])
 
     if dwp:
         return dwp, dwp['tracks']['items'][0]['added_at']
-    else:
-        raise ValueError("Couldn't find ' Discover Weekly' Playlist.")
+    raise ValueError("Error: Couldn't find Discover Weekly Playlist.")
 
 
-def is_already_archived(playlist_name):
+def is_already_archived(playlist_name: str) -> bool:
     with open('archived_playlists.txt', 'r') as f:
         content = f.read()
-
-    if playlist_name in content:
-        return True
-
-    return False
+    return playlist_name in content
 
 
-def create_archive_playlist(sp, user_id, playlist_name, description=None):
+def archive_discover_weekly_playlist(
+        spotify_instance: Spotify,
+        user_id: str,
+        playlist_name: str,
+        description: str = None,
+) -> dict:
+    if is_already_archived(playlist_name):
+        raise ValueError("Error: Discover Weekly playlist is already archived.")
 
-    if not is_already_archived(playlist_name):
-        # create new playlist
-        new_dwp = sp.user_playlist_create(user_id, playlist_name, description)
+    new_dwp = spotify_instance.user_playlist_create(user_id, playlist_name, description)
 
-        with open('archived_playlists.txt', 'w') as f:
-            f.write(playlist_name)
+    with open('archived_playlists.txt', 'w') as f:
+        f.write(playlist_name)
 
-        return new_dwp
-    else:
-        raise ValueError('Discover Weekly playlist is already archived.')
+    return new_dwp
 
 
-def main():
-
+def main() -> None:
     with open('archived_playlists.txt', 'a+'):
         pass
 
-    scope = "user-read-recently-played playlist-modify-public playlist-modify-private user-read-currently-playing user-library-modify playlist-read-private user-library-read"
-
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ.get('SPOTIFY_WEEKLY_SAVER_CLIENT_ID'),
-                                                   client_secret=os.environ.get('SPOTIFY_WEEKLY_SAVER_CLIENT_SECRET'),
-                                                   redirect_uri="http://localhost:8000/",
-                                                   scope=scope))
-    user_id = sp.me()['id']
+    scope = "user-read-recently-played " \
+            "playlist-modify-public " \
+            "playlist-modify-private " \
+            "user-read-currently-playing " \
+            "user-library-modify " \
+            "playlist-read-private " \
+            "user-library-read"
+    spotify_credentials = SpotifyOAuth(
+        client_id=os.environ.get('SPOTIFY_WEEKLY_SAVER_CLIENT_ID'),
+        client_secret=os.environ.get('SPOTIFY_WEEKLY_SAVER_CLIENT_SECRET'),
+        redirect_uri="http://localhost:8000/",
+        scope=scope,
+    )
+    spotify_instance = Spotify(auth_manager=spotify_credentials)
+    user_id = spotify_instance.me().get('id')
 
     # get Discover Weekly playlist and its creation date
-    dwp, created_at = get_discover_weekly_playlist(sp)
+    dwp, created_at = get_discover_weekly_playlist(spotify_instance)
 
     # create new dwp name
     new_dwp_name = f"{dwp['name']} - {created_at.split('T')[0]}"
@@ -70,26 +71,19 @@ def main():
 
     # create new playlist
     print('Creating new playlist...')
-    new_dwp = create_archive_playlist(sp, user_id, new_dwp_name)
+    new_dwp = archive_discover_weekly_playlist(spotify_instance, user_id, new_dwp_name)
 
     # add tracks from the main dwp to the new one
     print('Saving tracks...')
-    sp.playlist_add_items(new_dwp['id'], items=tracks)
+    spotify_instance.playlist_add_items(new_dwp['id'], items=tracks)
 
 
 if __name__ == '__main__':
-    main()
-    print('Done!')
-
-
-
-
-
-
-
-
-
-
-
-
-
+    try:
+        main()
+    except ValueError as e:
+        print(e)
+    except Exception as e:
+        print(e)
+    else:
+        print('Done!')
